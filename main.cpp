643d34fda55c75334b1c274bc8db61cec4d551d9
SkyyCivilK <74734468+SkyyCivilK@users.noreply.github.com>
2022-11-07 00:01:17 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+class Room{
+    public:
+    int studentCount;
+    struct Student{
+      FSctruct["Long", "Wide", "Round", "large"];
+      Shoes["Jordans", "Nike", "Converse All Star"];
+      Physical["buff", "built", "slim", "skinny"];
+    };
+}
+
+int main()
+{
+    
+
+    return 0;
+}
